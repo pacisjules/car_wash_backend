28bd1b491a7ac04e72e359a5506d6e0fea43fa48
Ishimwe Jules Pacis (Mediator) <51479761+pacisjules@users.noreply.github.com>
2022-07-11 22:56:11 +0200

    last db

--- configs/connection.py
+++ configs/connection.py
@@ -16,7 +16,7 @@ def DATABASE_URL(
     port: str       = "5432", #5432 always for postgress
     database: str   = "car_wash"
 ):
-    return str("postgres://nljrgdamgfpuxk:86b08808d0141eab85ac115f7cce5cd408f574728438d42d1ed3fa5add0df0fb@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d3q8kc6m257chd")
+    return str("postgresql://nljrgdamgfpuxk:86b08808d0141eab85ac115f7cce5cd408f574728438d42d1ed3fa5add0df0fb@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d3q8kc6m257chd")
 
 database = databases.Database(DATABASE_URL())
 
